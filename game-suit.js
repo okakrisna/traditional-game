21f23e857fd0bb4d77443d5cef87192fe17682c1
Oka Krisna <7776987-Okakrisna@users.noreply.gitlab.com>
2023-06-14 22:02:40 +0800

    challange chapter 4

--- game-suit.js
+++ game-suit.js
@@ -4,52 +4,56 @@ const guntingPlayer1 = document.querySelector(".btn-gunting-player1");
 const batuCom = document.querySelector(".btn-batu-com");
 const kertasCom = document.querySelector(".btn-kertas-com");
 const guntingCom = document.querySelector(".btn-gunting-com");
-let pilihanPlayer = [batuPlayer1, kertasPlayer1, guntingPlayer1];
-let pilihanCom = [batuCom, kertasCom, guntingCom];
+let playerPilihan = [batuPlayer1, kertasPlayer1, guntingPlayer1];
+let comPilihan = [batuCom, kertasCom, guntingCom];
 
-pilihanPlayer.forEach((pilihan) => {
-  pilihan.addEventListener("click", () => {
-    let pilihanComAcak = pilihanCom[Math.floor(Math.random() * 3)];
-
-    if (pilihanComAcak === batuCom) {
-      const hoverBatuCom = document.querySelector(".btn-batu-com");
-      hoverBatuCom.style.backgroundColor = "#d3cece";
-      hoverBatuCom.style.borderRadius = "10px";
-      setTimeout(() => {
-        hoverBatuCom.style.backgroundColor = "";
-        hoverBatuCom.style.borderRadius = "";
-      }, 3000);
-    } else if (pilihanComAcak === kertasCom) {
-      const hoverKertasCom = document.querySelector(".btn-kertas-com");
-      hoverKertasCom.style.backgroundColor = "#d3cece";
-      hoverKertasCom.style.borderRadius = "10px";
-      setTimeout(() => {
-        hoverKertasCom.style.backgroundColor = "";
-        hoverKertasCom.style.borderRadius = "";
-      }, 3000);
-    } else {
-      pilihanComAcak === guntingCom;
-      const hoverGuntingCom = document.querySelector(".btn-gunting-com");
-      hoverGuntingCom.style.backgroundColor = "#d3cece";
-      hoverGuntingCom.style.borderRadius = "10px";
-      setTimeout(() => {
-        hoverGuntingCom.style.backgroundColor = "";
-        hoverGuntingCom.style.borderRadius = "";
-      }, 3000);
-    }
-    //console.log(pilihan);
-    //console.log(pilihanComAcak);
-  });
-});
-
-const resultWinner1 = (player, com) => {
-  if (pilihanPlayer === pilihanCom) {
-    console.log("draw");
+const resultWinner = (player, com) => {
+  let pilihanPlayer = document.querySelector(".btn-batu-player1");
+  let result = document.querySelector(".text-versus");
+  let resultPic = document.querySelector(".resultPic");
+  if (player === com) {
+    result.remove(".text-versus");
+    resultPic.innerHTML =
+      "<img src='assets/assets/draw.png' alt='draw' class='resultPic' />";
   } else if (
     (player === "batu" && com === "gunting") ||
     (player === "kertas" && com === "batu") ||
     (player === "gunting" && com === "kertas")
   ) {
-    alert("win");
+    result.remove(".text-versus");
+    resultPic.innerHTML =
+      "<img src='assets/assets/playerwin.png' alt='draw' class='resultPic' />";
+  } else {
+    result.remove(".text-versus");
+    resultPic.innerHTML =
+      "<img src='assets/assets/comwin.png' alt='draw' class='resultPic' />";
   }
 };
+
+playerPilihan.forEach((pilihan) => {
+  pilihan.addEventListener("click", () => {
+    let pilihanComAcak = comPilihan[Math.floor(Math.random() * 3)];
+    const hoverBatuCom = document.querySelector(".btn-batu-com");
+    const hoverKertasCom = document.querySelector(".btn-kertas-com");
+    const hoverGuntingCom = document.querySelector(".btn-gunting-com");
+    if (pilihanComAcak.id === "batu") {
+      hoverBatuCom.style.backgroundColor = "#d3cece";
+      hoverBatuCom.style.borderRadius = "10px";
+    } else if (pilihanComAcak.id === "kertas") {
+      hoverKertasCom.style.backgroundColor = "#d3cece";
+      hoverKertasCom.style.borderRadius = "10px";
+    } else {
+      pilihanComAcak.id === "gunting";
+
+      hoverGuntingCom.style.backgroundColor = "#d3cece";
+      hoverGuntingCom.style.borderRadius = "10px";
+    }
+    resultWinner(pilihan.id, pilihanComAcak.id);
+  });
+});
+
+const refreshPage = () => {
+  location.reload();
+};
+const btnrefresh = document.querySelector(".btn-refresh-game");
+btnrefresh.addEventListener("click", refreshPage);
